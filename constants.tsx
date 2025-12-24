e58168fa95f534af2c62a0ac63da49bfd4f8500c
dinochkakhv-create <dinochkakhv@gmail.com>
2025-12-24 13:34:53 +0100

    🚀 Sovereign Sync: constants.tsx

---
+++ constants.tsx
@@ -0,0 +1,25 @@
+
+import { Service } from './types';
+
+export const SERVICES: Service[] = [
+  { id: '1', name: 'YouTube', icon: 'youtube', color: '#FF0000' },
+  { id: '2', name: 'Discord', icon: 'discord', color: '#5865F2' },
+  { id: '3', name: 'Telegram', icon: 'telegram', color: '#26A5E4' },
+  { id: '4', name: 'Roblox', icon: 'roblox', color: '#000000' },
+  { id: '5', name: 'Minecraft', icon: 'minecraft', color: '#62B037' },
+  { id: '6', name: 'Wikipedia', icon: 'wikipedia', color: '#FFFFFF' },
+  { id: '7', name: 'Twitter / X', icon: 'x', color: '#000000' },
+  { id: '8', name: 'Brawl Stars', icon: 'supercell', color: '#F8C400' },
+  { id: '9', name: 'Steam', icon: 'steam', color: '#00ADEE' },
+  { id: '10', name: 'Twitch', icon: 'twitch', color: '#9146FF' },
+  { id: '11', name: 'VPN Service', icon: 'lock', color: '#00FF41' },
+  { id: '12', name: 'TikTok', icon: 'tiktok', color: '#000000' },
+];
+
+export const BUREAUCRATIC_PREFIXES = [
+  "Согласно постановлению №",
+  "В соответствии с законом Яровой",
+  "На основании секретного приказа",
+  "По результатам мониторинга информационной среды",
+  "В целях защиты суверенитета цифрового пространства"
+];
